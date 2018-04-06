7f1a0c9752c7143b323bfc21654acbc33261e328
Lucas Brito <lucassantanabrito@icloud.com>
2018-04-06 17:03:04 -0300

    Foram criadas as telas do aplicativo.

---
+++ SuperSenha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperSenha
+//
+//  Created by Lucas Santana Brito on 06/04/2018.
+//  Copyright © 2018 Lucas Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
